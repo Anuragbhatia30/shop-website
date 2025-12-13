7f59322e3072be8ff3e6ee7544791952c8e771d3
Anuragbhatia30 <149615457+Anuragbhatia30@users.noreply.github.com>
2025-12-13 23:10:08 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+'use client'
+import Home from "./Home/page";
+
+export default function Page() {
+  return (
+    <div>
+      <Home />
+    </div>
+    
+  );
+}
